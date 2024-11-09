9e9bdd729b983f6a1c2ea992c33b83f6aeb77fd8
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2024-11-09 09:07:52 +0100

    Add tracing

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -5,40 +5,61 @@ use axum::http::{
     HeaderValue, Method,
 };
 use dotenv::dotenv;
-use sqlx::mysql::MySqlPoolOptions;
+use sqlx::{mysql::MySqlPoolOptions, MySqlPool};
 use tower_http::cors::CorsLayer;
 
 use pace_server::server::{app_state::AppState, router::create_router};
+
+use tracing::{error, info};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+async fn connect_to_database() -> MySqlPool {
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await;
+
+    if let Err(err) = pool {
+        error!("🔥 Failed to connect to the database: {:?}", err);
+        std::process::exit(1);
+    }
+
+    info!("Connection to the database is successful!");
+    pool.unwrap()
+}
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = match MySqlPoolOptions::new()
-        .max_connections(10)
-        .connect(&database_url)
-        .await
-    {
-        Ok(pool) => {
-            println!("✅Connection to the database is successful!");
-            pool
-        }
-        Err(err) => {
-            println!("🔥 Failed to connect to the database: {:?}", err);
-            std::process::exit(1);
-        }
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "pace_server=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app_state = AppState::new(connect_to_database().await);
+
+    let app = create_router(Arc::new(app_state));
+
+    let app = {
+        let cors = CorsLayer::new()
+            .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+            .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+            .allow_credentials(true)
+            .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+        app.layer(cors)
     };
 
-    let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
-        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
-        .allow_credentials(true)
-        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+    info!("Pace Server started successfully");
 
-    let app = create_router(Arc::new(AppState::new(pool.clone()))).layer(cors);
+    let bind_address = "0.0.0.0:8000";
+    let listener = tokio::net::TcpListener::bind(bind_address).await.unwrap();
+    info!("Start listening on {bind_address}");
 
-    println!("Pace Server started successfully");
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap()
 }
