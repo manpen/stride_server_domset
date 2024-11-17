02b662780eef4d8ff6f3b18b4dceada9df16f072
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2024-11-17 11:29:37 +0100

    Add new metadata columns

---
+++ migrations/20241116214022_metadata.down.sql
@@ -0,0 +1,13 @@
+-- Add down migration script here
+ALTER TABLE SolverRun
+    DROP COLUMN num_scheduled;
+
+ALTER TABLE Instance
+    DROP COLUMN min_deg,
+    DROP COLUMN max_deg,
+    DROP COLUMN med_deg,
+    DROP COLUMN num_ccs,
+    DROP COLUMN nodes_largest_cc,
+    DROP COLUMN planar,
+    DROP COLUMN diameter,
+    DROP COLUMN tree_width;
---
+++ migrations/20241116214022_metadata.up.sql
@@ -0,0 +1,15 @@
+-- Add up migration script here
+ALTER TABLE SolverRun
+    ADD COLUMN num_scheduled INT UNSIGNED;
+
+ALTER TABLE Instance
+    ADD COLUMN min_deg INT UNSIGNED,
+    ADD COLUMN max_deg INT UNSIGNED,
+    ADD COLUMN med_deg FLOAT,
+    ADD COLUMN num_ccs INT UNSIGNED,
+    ADD COLUMN nodes_largest_cc INT UNSIGNED,
+    ADD COLUMN planar BOOLEAN,
+    ADD COLUMN diameter INT UNSIGNED,
+    ADD COLUMN tree_width INT UNSIGNED;
+
+
