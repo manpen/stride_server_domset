7c12478fe0cd890b9183457493f807b062ca36de
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2024-11-17 18:35:03 +0100

    Exclude sqlx from db backup

--- tools/backup_db.sh
+++ tools/backup_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 docker-compose exec mysql /bin/bash -c \
-    'MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysqldump -u root $MYSQL_DATABASE' \
+    'MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysqldump -u root $MYSQL_DATABASE --add-drop-table --ignore-table $MYSQL_DATABASE._sqlx_migrations' \
     | bzip2 db_backup.sql.bz2
     
     
