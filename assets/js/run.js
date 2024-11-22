25d9f9c634b08858147440052f244ce68d59bd45
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2024-11-22 19:48:34 +0100

    Add draft of plotting

---
+++ assets/js/run.js
@@ -0,0 +1,247 @@
+function isValidUUID(uuid) {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(uuid);
+}
+
+const SOLVER = new URLSearchParams(window.location.search).get('solver');
+const RUN = new URLSearchParams(window.location.search).get('run');
+
+if (!isValidUUID(SOLVER)) {
+    alert('Invalid solver_uuid');
+    window.location.href = '/';
+}
+
+if (!isValidUUID(RUN)) {
+    alert('Invalid run_uuid');
+    window.location.href = '/';
+}
+
+const apiBase = '/api/';
+const apiTags = apiBase + 'tags';
+const apiInstances = apiBase + `instances?solver=${SOLVER}&run=${RUN}`;
+const apiSolverRunList = apiBase + `solver_run/list?solver=${SOLVER}&run=${RUN}`;
+
+function scoreFmt(value, row) {
+    if (value == "Infeasible") {
+        return "<span class='error'>Infeasible</span>";
+    }
+
+    const delta = row.raw.solution.score - row.raw.best_score;
+    if (delta === 0) {
+        return `<span class='optimal'>${value}</span>`;
+    }
+
+    return value;
+}
+
+function digitsFmt(value, row) {
+    value = value.toFixed(0);
+
+    if (value.length <= 3) {
+        return value;
+    }
+
+    while (value.length % 3) {
+        value = " " + value;
+    }
+
+    let result = "";
+    for (let i = 0; i < value.length; i += 3) {
+        result += value.substr(i, 3) + "'";
+    }
+
+    return result.slice(0, -1);
+}
+
+function populateTable(instances) {
+    let data = [];
+    instances.forEach(ins => {
+        row = {
+            'raw': ins,
+        };
+
+        function add_td(key, fmt = "num", if_unknown = "?", order_by_key = null) {
+            let value = ins[key];
+
+            if (value === null || value === undefined) {
+                value = if_unknown;
+            } else {
+                if (fmt == "num") {
+                    //
+                } else {
+                    value = value ? "✅" : "❌";
+                }
+            }
+
+            row[key] = value;
+        }
+
+        add_td("iid", "num", null, "id");
+        add_td("name");
+        add_td("nodes");
+        add_td("edges");
+
+        add_td("best_score", "num", "❓");
+
+
+        // score
+        {
+
+            let score = ins.solution.score;
+            if (score === null) {
+                row["score"] = ins.solution.error_code;
+                row["score_delta"] = "";
+            } else {
+                row["score"] = score;
+                row["score_delta"] = score - ins.best_score;
+            }
+        }
+
+        // runtime
+        {
+            let runtime = ins.solution.seconds_computed;
+            row["time"] = runtime.toFixed(runtime > 100 ? 0 : 2) + "s";
+        }
+
+        data.push(row);
+    });
+
+    $("#instances").bootstrapTable('load', data)
+}
+
+// setup header
+fetch(apiSolverRunList).then(response => response.json()).then(data => {
+    const run = data.runs[0];
+    const name = run.name ? run.name : `Run ${run.run_uuid}`;
+
+    document.querySelector("#run-name").innerText = name;
+    document.querySelector("#run-description").innerText = run.description;
+
+    const total_num = run.num_optimal + run.num_suboptimal + run.num_infeasible + run.num_error + run.num_timeout;
+    const total_width = (run.num_scheduled ? run.num_scheduled : total_num);
+
+    let stats_tbody = document.querySelector("#run-stats tbody");
+    stats_tbody.innerHTML = "";
+
+    function add_row(title, num, cls) {
+        let row = document.createElement("tr");
+        if (cls) { row.classList.add(cls); }
+
+        let title_elem = document.createElement("td");
+        title_elem.innerText = title;
+        row.appendChild(title_elem);
+
+        let num_elem = document.createElement("td");
+        num_elem.classList.add("num");
+        num_elem.innerText = num;
+        row.appendChild(num_elem);
+
+        let frac_elem = document.createElement("td");
+        frac_elem.classList.add("num");
+        frac_elem.innerText = (num / total_width * 100).toFixed(1) + "%";
+        row.appendChild(frac_elem);
+
+        stats_tbody.appendChild(row);
+    }
+
+    add_row("Optimal instances", run.num_optimal, "optimal");
+    add_row("Suboptimal instances", run.num_suboptimal, "");
+    add_row("Infeasible instances", run.num_infeasible, "error");
+    add_row("Timeout instances", run.num_timeout, "warning");
+    add_row("Error instances", run.num_error, "error");
+
+});
+
+function plotRuntime(instances) {
+    let solved = { x: [], y: [], mode: 'markers', type: 'scatter', name: "Solved" };
+    let unsolved = { x: [], y: [], mode: 'markers', type: 'scatter', name: "Unsolved" };
+
+    instances.forEach(ins => {
+        let runtime = ins.solution.seconds_computed;
+        if (runtime === null) { return; }
+
+        if (ins.solution.score !== null) {
+            solved.x.push(ins.nodes);
+            solved.y.push(runtime);
+        } else {
+            unsolved.x.push(ins.nodes);
+            unsolved.y.push(runtime);
+        }
+    });
+
+    const layout = {
+        xaxis: {
+            title: 'Nodes',
+            type: 'log',
+            autorange: true
+        },
+        yaxis: {
+            title: 'Runtime (s)',
+            type: 'log',
+            autorange: true
+        },
+    };
+
+    let elem = document.getElementById('runtime-plot');
+    Plotly.newPlot(elem, [solved, unsolved], layout);
+}
+
+
+function plotScore(instances) {
+    let solved = { x: [], y: [], text: [], mode: 'markers', type: 'scatter', name: "Solved" };
+
+    instances.forEach(ins => {
+        const score = ins.solution.score;
+        if (score === null) { return; }
+
+        solved.x.push(ins.nodes);
+        solved.y.push(score - ins.best_score);
+        solved.text.push(`Instance: ${ins.iid}<br>Nodes: ${ins.nodes}<br>Edges: ${ins.edges}<br>Score: ${score}<br>Best Score: ${ins.best_score}<br>Runtime: ${ins.solution.seconds_computed.toFixed(2)}s`);
+    });
+
+    const layout = {
+        xaxis: {
+            title: 'Nodes',
+            type: 'log',
+            autorange: true
+        },
+        yaxis: {
+            title: 'Score - Best Score',
+            autorange: true
+        },
+    };
+
+    let elem = document.getElementById('runtime-plot');
+    Plotly.newPlot(elem, [solved], layout);
+}
+
+function plot(instances) {
+    if (document.querySelector("#plot-type").value == "score") {
+        plotScore(instances);
+    } else {
+        plotRuntime(instances);
+    }
+}
+
+let instances = [];
+
+document.querySelector("#plot-type").addEventListener("change", () => {
+    if (instances.length > 0) {
+        plot(instances);
+    }
+});
+
+//$(function () {
+fetch(apiInstances)
+    .then(response => response.json())
+    .then(data => {
+        instances = data.results;
+        populateTable(instances);
+        plot(instances);
+    });
+
+document.querySelector("#breadcrumb-solver").href = "/runs.html?solver=" + SOLVER;
+//});
+
+
+
---
+++ assets/js/runs.js
@@ -0,0 +1,200 @@
+const SOLVER = new URLSearchParams(window.location.search).get('solver');
+function isValidUUID(uuid) {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(uuid);
+}
+
+if (!isValidUUID(SOLVER)) {
+    alert('Invalid solver UUID');
+    window.location.href = '/';
+}
+
+document.querySelector("h1").innerText = `Solver ${SOLVER}`;
+
+
+const apiBase = '/api/';
+const apiSolverRunList = apiBase + `solver_run/list?solver=${SOLVER}`;
+const apiSolverRunAnnotate = apiBase + `solver_run/annotate?solver=${SOLVER}`;
+
+function getRunUuid(elem) {
+    while (elem != document) {
+        if (elem.run_uuid) {
+            return elem.run_uuid;
+        }
+        elem = elem.parentNode;
+    }
+}
+
+
+function populateRuns(data) {
+    let tbody = document.getElementById("runs");
+    tbody.innerHTML = "";
+
+    for (run of data.runs) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.classList.add("run");
+        row.classList.add("m-3");
+
+        if (run.hide) {
+            row.classList.add("hidden");
+        }
+
+        row.id = `run-${run.run_uuid}`;
+        row.run_uuid = run.run_uuid;
+
+        let info = document.createElement("div");
+        info.classList.add("info");
+        info.classList.add("col-6");
+        info.classList.add("p-6");
+        function add_content(el, cls, text) {
+            let elem = document.createElement(el);
+            elem.className = cls;
+            elem.innerText = text;
+            info.appendChild(elem);
+            return elem;
+        }
+
+        const name = run.name ? run.name : run.run_uuid;
+
+        let h4 = add_content("h4", "name", name);
+        h4.addEventListener("click", (e) => {
+            const run = getRunUuid(e.target);
+            window.location.href = `/run.html?solver=${SOLVER}&run=${run}`;
+        });
+
+        let tools = add_content("div", "tools", "");
+        {
+            function add_tool(cls, text, handler) {
+                let a = document.createElement("a");
+                a.classList.add(cls);
+                a.innerText = text;
+                tools.appendChild(a);
+                if (handler) {
+                    a.addEventListener("click", handler);
+                }
+                return a;
+            }
+
+            add_tool("hide", run.hide ? "[unhide]" : "[hide]", (e) => {
+                const run = getRunUuid(e.target);
+                const run_elem = document.querySelector(`#run-${run}`);
+                const is_hidden = run_elem.classList.contains("hidden");
+                const new_value = !is_hidden;
+
+                if (new_value) {
+                    run_elem.classList.add("hidden");
+                    e.target.innerText = "[unhide]";
+                } else {
+                    run_elem.classList.remove("hidden");
+                    e.target.innerText = "[hide]";
+                }
+
+                fetch(`${apiSolverRunAnnotate}&run=${run}&hide=${new_value}`);
+            });
+
+            add_tool("edit_name", " [change name]", (e) => {
+                const run = getRunUuid(e.target);
+                const h4 = document.querySelector(`#run-${run} h4`);
+                const old_value = h4.innerText;
+                const new_value = prompt("Enter new name", old_value);
+
+                if (new_value && new_value != old_value) {
+                    fetch(`${apiSolverRunAnnotate}&run=${run}&name=${encodeURIComponent(new_value)}`);
+                    h4.innerText = new_value;
+                }
+            });
+
+            add_tool("edit_description", " [change description]", (e) => {
+                const run = getRunUuid(e.target);
+                let p = document.querySelector(`#run-${run} p.desc`);
+                const old_value = p.innerHTML;
+                const new_value = prompt("Enter new description", old_value);
+
+                if (new_value && new_value != old_value) {
+                    fetch(`${apiSolverRunAnnotate}&run=${run}&description=${encodeURIComponent(new_value)}`);
+                    p.innerText = new_value;
+                }
+            });
+
+        }
+
+
+        add_content("span", "created", run.created_at);
+        add_content("p", "desc", run.description);
+
+        row.appendChild(info);
+
+        ////
+
+        const total_num =
+            run.num_optimal + run.num_suboptimal + run.num_infeasible + run.num_error + run.num_timeout;
+
+        run.num_error + run.num_infeasible + run.num_valid + run.num_optimal;
+        const total_width = (run.num_scheduled ? run.num_scheduled : total_num);
+
+        let right_col = document.createElement("div");
+        right_col.classList.add("col-6");
+
+        let progress = document.createElement("div");
+        progress.classList.add("progress-stacked");
+        progress.classList.add("p-0");
+
+
+        function add_block(num, cls, title) {
+            if (num == 0) { return; }
+            let progress_outer = document.createElement("div");
+            progress_outer.className = "progress";
+            progress_outer.role = "progressbar";
+            progress_outer.style.width = `${num / total_width * 100}%`;
+
+
+            let inner_bar = document.createElement("div");
+            inner_bar.classList.add("progress-bar");
+            for (c of cls) {
+                inner_bar.classList.add(c);
+            }
+            inner_bar.innerHTML = title.replace("$", (num / total_width * 100).toFixed(1) + "%");
+            console.log(num, total_width, num / total_width * 100);
+
+            progress_outer.appendChild(inner_bar);
+            progress.appendChild(progress_outer);
+
+        }
+
+        add_block(run.num_optimal, ["bg-success"], "Opt:<br/>$");
+        add_block(run.num_suboptimal, [], "Subopt:<br/> $");
+        add_block(run.num_infeasible, ["bg-danger"], "Infeasible:<br/> $");
+        add_block(run.num_timeout, ["bg-warning"], "Timeout:<br/> $");
+        add_block(run.num_error, ["bg-danger"], "Error:<br/> $");
+        add_block(total_width - total_num, ["bg-info", "rogress-bar-striped"], "", "Scheduled:<br/> $");
+
+        right_col.appendChild(progress);
+
+        let right_info = document.createElement("div");
+        right_info.classList.add("info");
+        right_info.innerText = `Total: ${total_num} runs`;
+
+        right_col.appendChild(right_info);
+
+        row.appendChild(right_col);
+        tbody.appendChild(row);
+    }
+
+
+}
+
+function fetchRuns() {
+    opts = "";
+
+    if (document.getElementById("include_hidden").checked) {
+        opts += "&include_hidden=true";
+    }
+
+    fetch(`${apiSolverRunList}${opts}`)
+        .then(response => response.json())
+        .then(data => populateRuns(data));
+}
+
+fetchRuns();
+document.getElementById("include_hidden").addEventListener("change", fetchRuns);
