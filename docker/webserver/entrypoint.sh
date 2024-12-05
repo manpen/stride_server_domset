8c01e742ae27e58086fbca58721cb7635d26c619
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2024-12-05 18:43:00 +0100

    Allow to set cargo build options within docker

--- docker/webserver/entrypoint.sh
+++ docker/webserver/entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-
 export DATABASE_URL=$DATABASE_URL_IN_DOCKER
 
 . /root/.cargo/env
@@ -11,9 +10,17 @@ mkdir -p /target
 export CARGO_TARGET_DIR="/target"
 
 cd /srv
-sqlx migrate run
+
+mkdir -p /active
+
+# build and execute the server
+cargo build --bin server --release $STRIDE_BUILD
+mv /target/release/server /active/server
+rm -rf /target
+
+cd /srv
 
 while true; do
-    RUST_LOG="debug" cargo run --bin server --release
+    /active/server 
     sleep 0.5
 done
