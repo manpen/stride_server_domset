a7b9165c2379afc056685f30373ccdea10eb2ad7
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2024-12-05 18:27:27 +0100

    Add statistics to /api/status handler

--- src/server/handlers/status.rs
+++ src/server/handlers/status.rs
@@ -1,12 +1,30 @@
 use super::common::*;
-use crate::pace::PROBLEM_ID;
 
-pub async fn status_handler() -> impl IntoResponse {
-    let json_response = serde_json::json!({
-        "status": "success",
-        "server": "Pace Instance Server",
-        "problem": PROBLEM_ID,
-    });
+#[derive(Serialize)]
+struct Response {
+    status: &'static str,
+    num_instances: u64,
+    num_jobs: u64,
+    num_unique_solutions: u64,
+}
 
-    Json(json_response)
+pub async fn status_handler(
+    State(app_data): State<Arc<AppState>>,
+) -> HandlerResult<impl IntoResponse> {
+    let num_instances = sqlx::query_scalar::<_, i64>("SELECT COUNT(*) FROM Instance")
+        .fetch_one(app_data.db())
+        .await? as u64;
+    let num_jobs = sqlx::query_scalar::<_, i64>("SELECT COUNT(*) FROM Solution")
+        .fetch_one(app_data.db())
+        .await? as u64;
+    let num_unique_solutions = sqlx::query_scalar::<_, i64>("SELECT COUNT(*) FROM SolutionData")
+        .fetch_one(app_data.db())
+        .await? as u64;
+
+    Ok(Json(Response {
+        status: "ok",
+        num_instances,
+        num_jobs,
+        num_unique_solutions,
+    }))
 }
